cf187ab11f00e87a2eaf1684bba976e2bb736372
vanesantillana <vanessa.santillana@ucsp.edu.pe>
2019-02-19 20:05:22 -0500

    ultimas imagenes

--- wp-content/themes/mesmerize/footer.php
+++ wp-content/themes/mesmerize/footer.php
@@ -1,5 +1,11 @@
+	<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3827.4546620267906!2d-71.53206048542914!3d-16.401719488676264!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x91424a55c4e099d5%3A0xb64798c1831690dd!2sCompuplaza+Arequipa!5e0!3m2!1ses!2spe!4v1550536850408" width="100%" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
 	<?php mesmerize_get_footer_content(); ?>
 	</div>
 <?php wp_footer(); ?>
+
 </body>
+<script>
+	var copy=document.getElementsByClassName("copyright")[0].innerHTML = "©  2019 Desarrollado por {Multiviral}";
+	//alert(copy);
+</script>
 </html>
